661474fe2dfed01c8dfddbc1a1c6cf8ddf96bdae
Vitaliy Novozhilov <don@vitalino.ru>
2022-08-14 16:54:33 +0300

    Home work 3

---
+++ 3/Program.cs
@@ -0,0 +1,37 @@
+﻿Console.Clear();
+int N = 0;
+InputNByUser(ref N); 
+DoAndPrintResult(N);
+
+static void InputNByUser (ref int n)
+{
+    try
+    {
+        Console.Write("Введите N: ");
+        n = int.Parse(Console.ReadLine() ?? "");
+    }
+    catch (Exception exc)
+    {
+        Console.WriteLine($"Ошибка ввода данных! {exc.Message}");
+        return;
+    }
+}
+
+
+static void DoAndPrintResult (int n)
+{   
+    if (n == 0)
+        {
+            Console.WriteLine($"Перезапустите приложение, повторите ввод.");  
+        }
+        else
+        {
+            Console.WriteLine($"Введено число N = {n}, таблица кубов от 1 до N:");
+            int i = 1;
+            while (i<=n)
+        {   
+            Console.Write($"{Math.Pow(i,3)}, ");
+            i++;
+        }
+        }
+}
